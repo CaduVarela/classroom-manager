fcf8ef20452e469333cfc36ddd30c4d2e3da2e68
CaduVarela <caduvarela@gmail.com>
2024-11-04 14:42:11 -0300

    feat: server init

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+
+import { router } from './routes'
+
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => res.send('Hello World!'))
+app.use('/api', router)
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`))
