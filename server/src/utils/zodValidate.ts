3a0c88a6151cc0d537ebc45a12539b52e6bda6a3
Carlos Varela <caduvarela@gmail.com>
2024-11-04 22:12:24 -0300

    feat: zodValidate middleware

---
+++ server/src/utils/zodValidate.ts
@@ -0,0 +1,22 @@
+import { RequestHandler } from "express"
+import { ZodSchema, z } from "zod"
+
+export function zodValidate(zodSchema: ZodSchema<any>): RequestHandler {
+    return (req, res, next) => {
+        try {
+            const data = zodSchema.parse({
+                body: req.body,
+                params: req.params,
+                query: req.query,
+            })
+            next()
+        } catch (err: any) {
+            if (err instanceof z.ZodError) {
+                res.status(400).json(err)
+                return
+            }
+            res.status(400).json(err.issues)
+            return
+        }
+    }
+}
